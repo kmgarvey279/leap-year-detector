a8283076e9478d41d304d611b6ec4251c24279a2
Kevin Garvey and Marissa Perry <kevin@>
2019-04-02 15:09:05 -0700

    Add front-end and back-end which checks if year is divisible by 4

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+  return false;
+  }
+};
+
+
+$().ready(function() {
+  $("form#leap-year").submit(function(event){
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
